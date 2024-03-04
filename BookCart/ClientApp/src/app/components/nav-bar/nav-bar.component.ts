a3c382529c2f2170b67eb6cce36984b665b1e1b1
AnkitSharma-007 <ankit.1si10cs010@gmail.com>
2024-03-04 21:54:32 +0530

    code refactor

--- BookCart/ClientApp/src/app/components/nav-bar/nav-bar.component.ts
+++ BookCart/ClientApp/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,20 +1,19 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { User } from 'src/app/models/user';
-import { UserType } from 'src/app/models/usertype';
-import { Router } from '@angular/router';
-import { AuthenticationService } from 'src/app/services/authentication.service';
-import { UserService } from 'src/app/services/user.service';
-import { SubscriptionService } from 'src/app/services/subscription.service';
-import { Observable } from 'rxjs';
-import { WishlistService } from 'src/app/services/wishlist.service';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { User } from "src/app/models/user";
+import { UserType } from "src/app/models/usertype";
+import { Router } from "@angular/router";
+import { AuthenticationService } from "src/app/services/authentication.service";
+import { UserService } from "src/app/services/user.service";
+import { SubscriptionService } from "src/app/services/subscription.service";
+import { Observable, ReplaySubject, takeUntil } from "rxjs";
+import { WishlistService } from "src/app/services/wishlist.service";
 
 @Component({
-  selector: 'app-nav-bar',
-  templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  selector: "app-nav-bar",
+  templateUrl: "./nav-bar.component.html",
+  styleUrls: ["./nav-bar.component.scss"],
 })
 export class NavBarComponent implements OnInit, OnDestroy {
-
   userId;
   userDataSubscription: any;
   userData = new User();
@@ -22,38 +21,46 @@ export class NavBarComponent implements OnInit, OnDestroy {
   wishListCount$: Observable<number>;
   cartItemCount$: Observable<number>;
 
+  private destroyed$ = new ReplaySubject<void>(1);
+
   constructor(
     private router: Router,
     private authService: AuthenticationService,
     private userService: UserService,
     private subscriptionService: SubscriptionService,
-    private wishlistService: WishlistService) {
-
-    this.userId = localStorage.getItem('userId');
-    this.wishlistService.getWishlistItems(this.userId).subscribe();
-    this.userService.getCartItemCount(this.userId).subscribe((data: number) => {
-      this.subscriptionService.cartItemcount$.next(data);
-    });
+    private wishlistService: WishlistService
+  ) {
+    this.userId = localStorage.getItem("userId");
+    this.wishlistService
+      .getWishlistItems(this.userId)
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe();
+    this.userService
+      .getCartItemCount(this.userId)
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe((data: number) => {
+        this.subscriptionService.cartItemcount$.next(data);
+      });
   }
 
   ngOnInit() {
-
-    this.userDataSubscription = this.subscriptionService.userData.asObservable().subscribe(data => {
-      this.userData = data;
-    });
+    this.subscriptionService.userData
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe((data) => {
+        this.userData = data;
+      });
 
     this.cartItemCount$ = this.subscriptionService.cartItemcount$;
     this.wishListCount$ = this.subscriptionService.wishlistItemcount$;
   }
 
   ngOnDestroy() {
-    if (this.userDataSubscription) {
-      this.userDataSubscription.unsubscribe();
-    }
+    this.destroyed$.next();
+    this.destroyed$.complete();
   }
 
   logout() {
     this.authService.logout();
-    this.router.navigate(['/login']);
+    this.router.navigate(["/login"]);
   }
 }

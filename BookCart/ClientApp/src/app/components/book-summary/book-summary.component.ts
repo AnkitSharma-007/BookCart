0a548800340720035c7095debd06ef0ec75079a3
AnkitSharma-007 <ankit.1si10cs010@gmail.com>
2025-04-18 14:12:26 +0530

    code refactor

--- BookCart/ClientApp/src/app/components/book-summary/book-summary.component.ts
+++ BookCart/ClientApp/src/app/components/book-summary/book-summary.component.ts
@@ -1,84 +1,79 @@
-import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
 import {
-  GoogleGenerativeAI,
-  HarmBlockThreshold,
-  HarmCategory,
-} from "@google/generative-ai";
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  inject,
+  Input,
+  signal,
+} from "@angular/core";
 import { MatProgressSpinner } from "@angular/material/progress-spinner";
 import { MatButton } from "@angular/material/button";
 import { MatCard, MatCardContent } from "@angular/material/card";
+import { GoogleGenAI, HarmBlockThreshold, HarmCategory } from "@google/genai";
 
 @Component({
-    selector: "app-book-summary",
-    templateUrl: "./book-summary.component.html",
-    styleUrls: ["./book-summary.component.scss"],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [MatCard, MatCardContent, MatButton, MatProgressSpinner]
+  selector: "app-book-summary",
+  templateUrl: "./book-summary.component.html",
+  styleUrls: ["./book-summary.component.scss"],
+  imports: [MatCard, MatCardContent, MatButton, MatProgressSpinner],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BookSummaryComponent {
+  private cd = inject(ChangeDetectorRef);
   geminiInput = "";
   bookSummary = "";
+  showLoader = signal<boolean>(false);
 
   @Input() set bookTitle(title: string) {
     this.geminiInput = `Give me a summary of the book "${title}" in 300 words`;
     this.bookSummary = "";
   }
-  showLoader = false;
 
-  MODEL_NAME = "gemini-1.5-flash";
-  API_KEY = "AIzaSyB88VzQVRWpEl5UKPUvzVZ8dIlYR-FV8gg";
-
-  generationConfig = {
-    temperature: 1,
-    topP: 0.95,
-    topK: 64,
-    maxOutputTokens: 8192,
+  MODEL_NAME = "gemini-2.0-flash";
+  API_KEY = "YOUR_API_KEY"; // Replace with your actual API key
+  genAI = new GoogleGenAI({ apiKey: this.API_KEY });
+  config = {
+    topP: 0.6,
+    topK: 40,
+    temperature: 0.8,
+    safetySettings: [
+      {
+        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE, // Block none
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE, // Block few
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE, // Block none
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE, // Block none
+      },
+    ],
     responseMimeType: "text/plain",
   };
-  safetySettings = [
-    {
-      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    },
-    {
-      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    },
-    {
-      category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    },
-    {
-      category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    },
-  ];
 
   async fetchSummaryFromGemini() {
-    const genAI = new GoogleGenerativeAI(this.API_KEY);
-    const model = genAI.getGenerativeModel({
-      model: this.MODEL_NAME,
-      generationConfig: this.generationConfig,
-      safetySettings: this.safetySettings,
-    });
-
     try {
-      this.showLoader = true;
+      this.showLoader.set(true);
       this.bookSummary = "";
-      const parts = [{ text: this.geminiInput }];
 
-      const result = await model.generateContentStream({
-        contents: [{ role: "user", parts }],
-        generationConfig: this.generationConfig,
-        safetySettings: this.safetySettings,
+      const response = await this.genAI.models.generateContentStream({
+        model: this.MODEL_NAME,
+        config: this.config,
+        contents: [{ role: "user", parts: [{ text: this.geminiInput }] }],
       });
 
-      this.showLoader = false;
+      this.showLoader.set(false);
 
-      for await (const chunk of result.stream) {
-        const chunkText = chunk.text();
-        this.bookSummary = this.bookSummary.concat(chunkText);
-        console.log(chunkText);
+      for await (const chunk of response) {
+        this.bookSummary = this.bookSummary.concat(chunk.text);
+        // this hack is required to trigger CD as the response is streamed.
+        this.cd.detectChanges();
       }
     } catch (e) {
       console.log("An error Occurred: ", e);
